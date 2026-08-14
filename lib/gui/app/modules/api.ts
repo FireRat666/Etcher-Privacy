d82a633fb25e98e39b462eca653d641084c9314a
FireRat <27763388+FireRat666@users.noreply.github.com>
2026-08-14 14:15:19 +1000

    Scoped the PATH="/usr/bin:/bin" prefix directly to sha256sum

--- lib/gui/app/modules/api.ts
+++ lib/gui/app/modules/api.ts
@@ -78,7 +78,7 @@ async function spawnChild(
 				argv = [
 					'/bin/bash',
 					'-c',
-					'PATH="/usr/bin:/bin" && echo "$2  $1" | sha256sum -c --status && exec "$1" "${@:3}"',
+					'echo "$2  $1" | PATH="/usr/bin:/bin" sha256sum -c --status && exec "$1" "${@:3}"',
 					'etcher-util',
 					tmpBin,
 					digest,
